db630499418100f066e49fac6be9e70be7f10ecb
dvkonyukhov <w1356@yandex.ru>
2022-06-29 22:32:43 +0300

    notify 2 экземпляра

--- src/dz4/waitnotify/WaitNotifyTest.java
+++ src/dz4/waitnotify/WaitNotifyTest.java
@@ -2,16 +2,20 @@ package dz4.waitnotify;
 
 public class WaitNotifyTest {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         Message msg = new Message("обработать");
         Waiter waiter = new Waiter(msg);
-        new Thread(waiter,"waiter").start();
+        new Thread(waiter,"waiter1").start();
 
         Waiter waiter1 = new Waiter(msg);
-        new Thread(waiter1, "waiter1").start();
+        new Thread(waiter1, "waiter2").start();
 
         Notifier notifier = new Notifier(msg);
-        new Thread(notifier, "notifier").start();
+        new Thread(notifier, "notifier1").start();
+
+        Thread.sleep(4000);
+        Notifier notifier1 = new Notifier(msg);
+        new Thread(notifier, "notifier2").start();
     }
 
 }
