d93f3c6ce4efc605cc9dcd74b31ea6a1bc50886c
dvkonyukhov <w1356@yandex.ru>
2022-06-29 17:26:49 +0300

    многопоточность wait notify

---
+++ src/dz4/waitnotify/Notifier.java
@@ -0,0 +1,26 @@
+package dz4.waitnotify;
+
+public class Notifier implements Runnable {
+
+    private final Message msg;
+
+    public Notifier(Message msg) {
+        this.msg = msg;
+    }
+
+    @Override
+    public void run() {
+        String name = Thread.currentThread().getName();
+        System.out.println(name + " стартовал");
+        try {
+            Thread.sleep(1000);
+            synchronized (msg) {
+                msg.setMsg(name + " поток Notifier отработал");
+                //msg.notify();
+                 msg.notifyAll();
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/dz4/waitnotify/Waiter.java
@@ -0,0 +1,25 @@
+package dz4.waitnotify;
+
+public class Waiter implements Runnable{
+
+    private final Message msg;
+
+    public Waiter(Message msg){
+        this.msg = msg;
+    }
+
+    @Override
+    public void run() {
+        String name = Thread.currentThread().getName();
+        synchronized (msg) {
+            try{
+                System.out.println(name + " ждем вызов метода notify...");
+                msg.wait();
+            }catch(InterruptedException e){
+                e.printStackTrace();
+            }
+            System.out.println(name + " был вызов метода notify...");
+            System.out.println(name + " : " + msg.getMsg());
+        }
+    }
+}
