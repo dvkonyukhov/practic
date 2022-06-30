11a6de36f78777997533dc49d4bc90ece7da796a
dvkonyukhov <w1356@yandex.ru>
2022-06-30 17:03:27 +0300

    добавил ввод нескольких команд с консоли

--- src/dz4/files/FileCopyTest.java
+++ src/dz4/files/FileCopyTest.java
@@ -37,7 +37,7 @@ public class FileCopyTest {
 
     private static void writeText(File file) {
         try (FileOutputStream fileOutputStream = new FileOutputStream(file)) {
-            StringBuffer stringBuffer = new StringBuffer();
+            StringBuilder stringBuffer = new StringBuilder();
 
             for (int i = 0; i < 15; i++) {
                 stringBuffer.append(i);
@@ -54,59 +54,51 @@ public class FileCopyTest {
 
         Message message = new Message("start");
 
-        Thread threadCopy = new Thread(new Runnable() {
-            @Override
-            public void run() {
+        Thread threadCopy = new Thread(() -> {
 
-                File fileDest = new File(fileDestName);
+            File fileDest = new File(fileDestName);
 
-                try (FileInputStream fileInputStream = new FileInputStream(file);
-                     FileOutputStream fileOutputStream = new FileOutputStream(fileDest)) {
+            try (FileInputStream fileInputStream = new FileInputStream(file);
+                 FileOutputStream fileOutputStream = new FileOutputStream(fileDest)) {
 
-                    int sizeOfBuffer = 5;
-                    int length;
-                    byte[] buffer = new byte[sizeOfBuffer];
-                    long lengthOfSourceFile = file.length();
-                    long step = 0;
-                    while ((length = fileInputStream.read(buffer)) > 0) {
-                        fileOutputStream.write(buffer, 0, length);
-                        step = step + sizeOfBuffer;
-                        System.out.println("Скопировано: " + (int) ((double) step / (double) lengthOfSourceFile * 100) + " процентов");
-                        Thread.sleep(4000);
-                        synchronized (message) {
-                            if (message.getMsg().equals("stop")) {
-                                System.out.println("Копирование прервано пользователем");
-                                fileOutputStream.close();
-                                fileDest.delete();
-                                break;
-                            }
-                        }
-                    }
-                    message.setMsg("stop");
-
-                } catch (IOException e) {
-                    e.printStackTrace();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-
-            }
-        });
-
-        Thread threadListenerInput = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                String commandStop = "stop";
-                Scanner scanner = new Scanner(System.in);
-                while (true) {
-                    String textUser = scanner.nextLine();
+                int sizeOfBuffer = 5;
+                int length;
+                byte[] buffer = new byte[sizeOfBuffer];
+                long lengthOfSourceFile = file.length();
+                long step = 0;
+                while ((length = fileInputStream.read(buffer)) > 0) {
+                    fileOutputStream.write(buffer, 0, length);
+                    step = step + sizeOfBuffer;
+                    System.out.println("Скопировано: " + (int) ((double) step / (double) lengthOfSourceFile * 100) + " процентов");
+                    Thread.sleep(4000);
                     synchronized (message) {
-                        if (textUser.equals(commandStop) || message.getMsg().equals(commandStop)) {
-                            message.setMsg(textUser);
-                        }
-                        {
+                        if (message.getMsg().equals("stop")) {
+                            System.out.println("Копирование прервано пользователем");
+                            fileOutputStream.close();
+                            fileDest.delete();
                             break;
                         }
+                    }
+                }
+                message.setMsg("stop");
+
+            } catch (IOException | InterruptedException e) {
+                e.printStackTrace();
+            }
+
+        });
+
+        Thread threadListenerInput = new Thread(() -> {
+            String commandStop = "stop";
+            Scanner scanner = new Scanner(System.in);
+            while (true) {
+                String textUser = scanner.nextLine();
+                synchronized (message) {
+                    if (textUser.equals(commandStop) || message.getMsg().equals(commandStop)) {
+                        message.setMsg(textUser);
+                    }
+                    {
+                        break;
                     }
                 }
             }
